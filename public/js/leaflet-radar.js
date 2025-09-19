6ea1a97926116a749b9fd141b2e032bc48ed25ce
atomlabor <46759744+atomlabor@users.noreply.github.com>
2025-09-19 11:22:27 +0200

    Update leaflet-radar.js

--- public/js/leaflet-radar.js
+++ public/js/leaflet-radar.js
@@ -1,12 +1,10 @@
-// TODO add refresh (reload time layers)
-// TODO add buffer time to load layers where radar turned on
 L.Control.Radar = L.Control.extend({
     // Updated radar source - using direct tile layer instead of WMS
     NEXRAD_URL: 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/{z}/{x}/{y}.png',
     isPaused: false,
     timeLayerIndex: 0,
     timeLayers: [],
-    radarLayer: null, // Store main radar layer
+    radarLayer: null, 
     options: {
         position: `topright`,
         opacity: 0.8,
@@ -20,14 +18,14 @@ L.Control.Radar = L.Control.extend({
     },
     onAdd: function (map) {
         this.map = map;
-        // setup control container
+ 
         this.container = L.DomUtil.create(`div`, "leaflet-radar");
         L.DomEvent.disableClickPropagation(this.container);
         L.DomEvent.on(this.container, `control_container`, function (e) {
             L.DomEvent.stopPropagation(e);
         });
         L.DomEvent.disableScrollPropagation(this.container);
-        // add control elements within container
+
         checkbox_div = L.DomUtil.create(
             `div`,
             `leaflet-radar-toggle`,
@@ -40,10 +38,9 @@ L.Control.Radar = L.Control.extend({
         this.checkbox.onclick = () => this.toggle();
         checkbox_div.appendChild(this.checkbox);
         let checkbox_label = document.createElement(`span`);
-        checkbox_label.innerText = `usa radar`;
+        checkbox_label.innerText = `us rain radar`;
         checkbox_div.appendChild(checkbox_label);
         
-        // Removed slider creation code
         
         this.timestamp_div = L.DomUtil.create(
             `div`,
@@ -65,7 +62,7 @@ L.Control.Radar = L.Control.extend({
         this.timestamp_div.innerHTML = this.timeLayers[index].timestamp;
     },
     setDisabled: function (disabled) {
-        // Removed slider disable logic
+
         this.timestamp_div.innerText = ``;
     },
     toggle: function () {
@@ -75,31 +72,31 @@ L.Control.Radar = L.Control.extend({
             return;
         }
         this.setDisabled(false);
-        // Create simple radar layer with new tile source
+ 
         this.createRadarLayer();
         
-        // Removed slider max setting and slider input event listener
+
         
         this.timeLayerIndex = 0;
         this.isPaused = false;
         this.setTransitionTimer();
     },
     createRadarLayer: function() {
-        // Remove existing layers
+  
         this.removeLayers();
         
-        // Create single radar layer with new tile source
+  
         this.radarLayer = L.tileLayer(this.NEXRAD_URL, {
             opacity: this.options.opacity,
             attribution: 'Weather radar © IEM, Iowa State University'
         });
         
-        // Add to map
+      
         this.radarLayer.addTo(this.map);
         
-        // Create dummy time layers for compatibility with existing UI
+  
         this.timeLayers = [{
-            timestamp: 'Live Radar',
+            timestamp: 'live radar on',
             tileLayer: this.radarLayer
         }];
         
@@ -131,7 +128,7 @@ L.Control.Radar = L.Control.extend({
         }
     },
     addLayers: function () {
-        // Updated for single layer approach
+ 
         if (this.radarLayer && !this.map.hasLayer(this.radarLayer)) {
             this.radarLayer.addTo(this.map);
         }
@@ -148,8 +145,7 @@ L.Control.Radar = L.Control.extend({
         this.timeLayerIndex = 0;
     },
     generateLayers: function () {
-        // Simplified - just return single layer for now
-        // This maintains compatibility with the existing radar control UI
+   
         let timeLayers = [];
         if (this.radarLayer) {
             timeLayers.push({
